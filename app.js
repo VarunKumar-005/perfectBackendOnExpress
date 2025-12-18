1e425984f0ee450ca816baf422ed8888fb8b1da6
Varun kumar S N <devilhunter12990@gmail.com>
2025-12-18 21:43:55 +0530

    Add movie routes to app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes.js';
+import movieRoutes from './routes/movieRoute.js'; // Import movie routes
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 
 app.use('/auth', authRoutes);
+app.use('/movies',movieRoutes); // Register movie routes
 
 export default app;
