93e356260cb856864ca79630e13dea89426f043b
Varun kumar S N <devilhunter12990@gmail.com>
2025-12-18 15:11:09 +0530

    Remove timestamps from user schema
    Removed timestamps option from user schema.

--- models/User.js
+++ models/User.js
@@ -4,8 +4,6 @@ const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true}
-}, {
-    timestamps: true
 });
 
 const User = mongoose.model("User", userSchema);
